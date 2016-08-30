1844fce8b9df4a1c6a4cf5c44307032b532b6b37
Minolo <minolo3ds@gmail.com>
2016-08-30 12:55:27 +0200

    Add trainer
    trainer.py implements the function *train* that receives the list of ham
    and spam files and generates a dictionary with the training data

---
+++ src/trainer.py
@@ -0,0 +1,74 @@
+import logging
+import argparse
+import pickle
+import itertools
+import collections
+
+from tokenizer import tokenize
+
+def train(ham_files, spam_files):
+    result = {}
+
+    # Tokenize files
+    ham_files_generator  = (tokenize(f) for f in ham_files)
+    spam_files_generator = (tokenize(f) for f in spam_files)
+
+    # Flatten lists
+    ham_files_all_tokens  = list(itertools.chain.from_iterable(ham_files_generator))
+    spam_files_all_tokens = list(itertools.chain.from_iterable(spam_files_generator)) 
+
+    # Count word occurrences
+    ham_tokens_counter  = collections.Counter(ham_files_all_tokens)
+    spam_tokens_counter = collections.Counter(spam_files_all_tokens)
+
+    # Fill result
+    number_ham  = len(ham_files )
+    number_spam = len(spam_files)
+    total_files = number_ham + number_spam
+    result["p-ham"]  = number_ham  / total_files
+    result["p-spam"] = number_spam / total_files
+    result["dict-ham"]  = dict([(word, occurrences / number_ham ) for (word, occurrences) in dict(ham_tokens_counter ).items()])
+    result["dict-spam"] = dict([(word, occurrences / number_spam) for (word, occurrences) in dict(spam_tokens_counter).items()])
+
+    return result
+
+def main():
+
+    # Define the program arguments for the parser
+    argpar = argparse.ArgumentParser()
+    argpar.add_argument("-a", "--ham-list" , type=str, required=True, help="Ham file list" )
+    argpar.add_argument("-s", "--spam-list", type=str, required=True, help="Spam file list")
+    argpar.add_argument("-o", "--output"   , type=str, required=True, help="Output file"   )
+
+    # Parse the arguments
+    args = argpar.parse_args()
+
+    # Read file lists and remove empty lines
+    try:
+        with open(args.ham_list, "rb") as ham_list:
+            ham_files = ham_list.read().splitlines()
+            ham_files = [line for line in ham_files if line]
+
+        with open(args.spam_list, "rb") as spam_list:
+            spam_files = spam_list.read().splitlines()
+            spam_files = [line for line in spam_files if line]
+
+    except Exception as e:
+        logging.error(e)
+        exit(-1)
+
+    # Build the dictionary with the training data
+    result = train(ham_files, spam_files)
+    print(result)
+
+    # Save the result in the output file
+    try:
+        with open(args.output, "wb") as output_file:
+            pickle.dump(result, output_file)
+
+    except Exception as e:
+        logging.error(e)
+        exit(-1)
+
+if __name__ == "__main__":
+    main()
