ed2f296bd9949b594568c4f50272318a7b4dcdcb
Carlos Falgueras García <carlosfg@riseup.net>
2016-08-31 16:54:26 +0200

    launcher: Imporve launcher output
    Launcher must returns the values of the modified parameters along with the
    confusion matrix.

--- src/launcher.sh
+++ src/launcher.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-TMP_DIR=$(mktemp)
+DATA_DIR="../res/dsets"
+
+TMP_DIR=$(mktemp -d)
 EV_HAM="$TMP_DIR/ev_ham"
 EV_SPAM="$TMP_DIR/ev_spam"
 TR_HAM="$TMP_DIR/tr_ham"
@@ -13,23 +15,28 @@ TRAINER="$PYTHON trainer.py"
 EVALUATOR="$PYTHON evaluator.py"
 
 ev_perc=0.25
-tr_perc=0.75
+tr_perc=0.74
 token="basic"
 umbral=0.9
 
+not_def_parms=()
 while getopts "e:t:k:u:" opt; do
 	case $opt in
 	e)
 		ev_perc=$OPTARG
+		not_def_parms+=($ev_perc)
 		;;
 	t)
 		tr_perc=$OPTARG
+		not_def_parms+=($tr_perc)
 		;;
 	k)
 		token=$OPTARG
+		not_def_parms+=($token)
 		;;
 	u)
 		umbral=$OPTARG
+		not_def_parms+=($umbral)
 		;;
 	esac
 done
@@ -42,6 +49,8 @@ mkfifo $TR_DATA
 
 $SPLITTER -e$ev_perc -t$tr_perc -d$DATA_DIR $EV_HAM $EV_SPAM $TR_HAM $TR_SPAM &\
 $TRAINER -a$TR_HAM -s$TR_SPAM -o$TR_DATA &\
-$EVALUATOR -a$EV_HAM -s$EV_SPAM -t$TR_DATA -m
+cmd=$($EVALUATOR -a$EV_HAM -s$EV_SPAM -t$TR_DATA -m)
 
-rm -fr $(TMP_DIR) >& /dev/null
+echo "\"${not_def_parms[@]}\" $cmat"
+
+rm -fr $TMP_DIR >& /dev/null
