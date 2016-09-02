4a6557fe1bb1ca5cd486920216421b2fcecc8a78
Carlos Falgueras García <carlosfg@riseup.net>
2016-09-02 15:43:14 +0200

    tokenizer: Fix token regexp
    Fix token search and add some new tokenize methods

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,15 +2,18 @@ import logging
 import re
 
 regexs = {
-    "basic": re.compile("\w+"),
-    "r1cw"  : re.compile("\w\w+")
+    "basic"    : re.compile("\w+"),
+    "word2"    : re.compile("\w\w+"),
+    "alphnum2" : re.compile("\w*[a-z_]{2}\w*"),
+    "alphnum3" : re.compile("\w*[a-z_]{3}\w*"),
+    "alph3"    : re.compile("[a-z_]{3}[a-z_]*"),
     }
 
 def tokenize(file_path, token_type):
     try:
         with open(file_path, "rb") as file:
             mail = file.read().decode("utf8", "ignore")
-            mail = mail.replace("\n", "").lower()
+            mail = mail.replace("\n", " ").lower()
             return list(set(regexs[token_type].findall(mail)))
     except Exception as e:
         logging.error(e)
