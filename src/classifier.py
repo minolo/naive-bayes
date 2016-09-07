ba12d5063a996bd55b335350a396c961caeb3a31
Minolo <minolo3ds@gmail.com>
2016-09-07 19:11:35 +0200

    Implement new evaluation algorithm
    Fixed the evaluator to make it compatible with the changes made to the trainer

--- src/classifier.py
+++ src/classifier.py
@@ -5,45 +5,30 @@ import math
 
 from tokenizer import tokenize
 
-def calculate_token_probability(mail_tokens, word, probability):
-    if word in mail_tokens:
-        return probability
-    else:
-        return 1 - probability
-
 def classify(mail_path, training_data, tk_type):
 
     # Tokenize mail
     mail_tokens = tokenize(mail_path, token_type=tk_type)
 
-    # Calculate conditional probabilities
-    #p_ham_list  = [calculate_token_probability(mail_tokens, word, probability) for (word, probability) in training_data["dict-ham" ].items()]
-    #p_spam_list = [calculate_token_probability(mail_tokens, word, probability) for (word, probability) in training_data["dict-spam"].items()]
+    # Gather logarithms
+    logarithms = [training_data[token] for token in mail_tokens if token in training_data]
 
-    p_ham_list  = [training_data["dict-ham" ][token] for token in mail_tokens if token in training_data["dict-ham" ]]
-    p_spam_list = [training_data["dict-spam"][token] for token in mail_tokens if token in training_data["dict-spam"]]
-
-    # Apply Bayes' theorem
-    log_p_ham  = math.log(training_data["p-ham" ])
-    log_p_spam = math.log(training_data["p-spam"])
-    log_p_ham_list  = [math.log(h) for h in p_ham_list ]
-    log_p_spam_list = [math.log(s) for s in p_spam_list]
-
-    exponent = log_p_spam + sum(log_p_spam_list) - log_p_ham - sum(log_p_ham_list)
+    # Calculate exponent
+    exponent = sum(logarithms)
 
     # If the exponent has a low absolute value, use the formula directly
     if abs(exponent) < 100:
-        p_ham_mail = 1 / (1 + math.pow(math.e, exponent))
+        probability = 1 / (1 + math.pow(math.e, exponent))
 
     # Otherwise, set p_ham_mail to 0 if it is positive and to 1 if it is negative (avoids range errors with math.pow)
     else:
-        p_ham_mail = max(min(-exponent, 1), 0)
+        probability = max(min(-exponent, 1), 0)
 
     # Classify the mail
-    if p_ham_mail > 0.5:
-        return "ham"
-    else:
+    if probability > 0.5:
         return "spam"
+    else:
+        return "ham"
 
 def main():
 
